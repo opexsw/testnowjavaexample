370e5f846a0acac5f875ae03d80425f880d88a36
tahirstamboli <tahirstamboli@hotmail.com>
2018-09-27 19:12:42 +0530

    Offline changes

--- src/test/java/com/java/cukes/Hooks.java
+++ src/test/java/com/java/cukes/Hooks.java
@@ -1,9 +1,9 @@
 /* 
-* File Name: Hooks.java
-* Copyright 2015, Opex Software
-* Apache License, Version 2.0
-* This file contains the setup and teardown methods, browser initializations and screenshot functionality
-*/
+ * File Name: Hooks.java
+ * Copyright 2015, Opex Software
+ * Apache License, Version 2.0
+ * This file contains the setup and teardown methods, browser initializations and screenshot functionality
+ */
 
 package com.java.cukes;
 
@@ -11,21 +11,21 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.lang.StringUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.opera.OperaDriverService;
 import org.openqa.selenium.opera.OperaOptions;
 import org.openqa.selenium.remote.CapabilityType;
@@ -43,155 +43,207 @@ public class Hooks {
 
 	public static WebDriver driver;
 	private String browser;
+	private String webDriverPath;
 	private UPAReporter reporter;
 
 	// Contains declaration of all browsers (FF, Chrome, IE, Opera, Android)
 	// This method is a hook which runs before every test
 	@Before
 	public void beforeEach() throws IOException, URISyntaxException {
-		browser = System.getenv("BROWSER");
-		if (browser == null) {
-			browser = "firefox";
-		}
-		System.out.println("Browser selected is " + browser);
-		if (browser.equalsIgnoreCase("chrome")) {
-			driver = new ChromeDriver();
-			driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
-			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-			driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
-			driver.manage().window().maximize();
-		} else if (browser.equalsIgnoreCase("device")) {
-			// driver = new ChromeDriver();
-			// driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
-			// driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-			// driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
-			// driver.manage().window().maximize();
-
-			String deviceName = System.getenv("VERSION");
-			deviceName = deviceName.replace("_", " ");
-			Map<String, String> mobileEmulation = new HashMap<String, String>();
-			mobileEmulation.put("deviceName", deviceName);
-			
-			Map<String, Object> chromeOptions = new HashMap<String, Object>();
-			chromeOptions.put("mobileEmulation", mobileEmulation);
-			DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-			capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-			driver = new ChromeDriver(capabilities);
-			driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
-			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-			driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
-
-		} else if (browser.equalsIgnoreCase("ie")) {
-			DesiredCapabilities cap = new DesiredCapabilities();
-			cap.setJavascriptEnabled(true);
-			cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-			driver = new RemoteWebDriver(new URL("http://localhost:5555"),cap);
-			driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
-			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-			driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
-			driver.manage().window().maximize();
-		} else if (browser.equalsIgnoreCase("opera")) {
-			DesiredCapabilities cap = DesiredCapabilities.operaBlink();
-			cap.setBrowserName("opera");
-			OperaOptions options = new OperaOptions();
-			options.setBinary("/usr/bin/opera");
-			options.addArguments("--ignore-certificate-errors");
-			cap.setCapability(OperaOptions.CAPABILITY, options);			
-			OperaDriverService service = new OperaDriverService.Builder()
-					.usingDriverExecutable(new File("/usr/local/bin/operadriver"))					
-					.usingAnyFreePort().build();
-			service.start();			
-			driver = new RemoteWebDriver(service.getUrl(),cap);
-			driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
-			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-			driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
-			driver.manage().window().maximize();
-		} else if (browser.equalsIgnoreCase("android")) {
-			driver = new RemoteWebDriver(DesiredCapabilities.android());
-		} else {
-			FirefoxProfile profile = new FirefoxProfile();
-			if (System.getenv("IS_UPA").equalsIgnoreCase("true")) {
-				//File netexport = new File("/home/vagrant/testNow/TestNow/testnowjavaexample/src/test/resources/har/netExport-0.8.xpi");
-				//File firebug = new  File("/home/vagrant/testNow/TestNow/testnowjavaexample/src/test/resources/har/firebug-2.0.13.xpi");
-				File netexport = new File(this.getClass().getClassLoader().getResource("har/netExport-0.8.xpi").toURI());
-				File firebug = new File(this.getClass().getClassLoader().getResource("har/firebug-2.0.13.xpi").toURI());
-				profile.addExtension(netexport);
-				profile.addExtension(firebug);
-				profile.setPreference("app.update.enabled", false);
-				String domain = "extensions.firebug.";
-				profile.setPreference(domain + "currentVersion", "2.0.13");
-				profile.setPreference(domain + "allPagesActivation", "on");
-				profile.setPreference(domain + "defaultPanelName", "net");
-				profile.setPreference(domain + "net.enableSites", true);
-				profile.setPreference(domain + "netexport.alwaysEnableAutoExport", true);
-				profile.setPreference(domain + "netexport.showPreview", false);
-				File harFolder = new File(System.getProperty("user.dir") + "/target/reports/har/");
-				profile.setPreference(domain + "netexport.defaultLogDir", harFolder.getAbsolutePath());
-			}
-			driver = new FirefoxDriver(profile);
-			driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
-			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-			driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
-			driver.manage().window().maximize();
-			reporter = new UPAReporter();
-		}
 		try {
+
+			browser = System.getenv(Constants.BROWSER);
+			webDriverPath = System.getenv(Constants.WEB_DRIVER_PATH);
+			System.out.println("Browser selected is " + browser);
+			if (Browser.CHROME.getBrowser().equalsIgnoreCase(browser)) {
+				System.setProperty("webdriver.chrome.driver", StringUtils
+						.isNotEmpty(webDriverPath) ? webDriverPath
+						: "/usr/local/bin/chromedriver");
+				/*
+				 * ChromeOptions chromeOptions = new ChromeOptions();
+				 * chromeOptions.addArguments("--no-sandbox");
+				 */
+				driver = new ChromeDriver();
+				driver.manage().timeouts()
+						.pageLoadTimeout(120, TimeUnit.SECONDS);
+				driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+				driver.manage().timeouts()
+						.setScriptTimeout(60, TimeUnit.SECONDS);
+				driver.manage().window().maximize();
+			} else if (Browser.DEVICE.getBrowser().equalsIgnoreCase(browser)) {
+				String deviceName = System.getenv("VERSION");
+				deviceName = deviceName.replace("_", " ");
+				Map<String, String> mobileEmulation = new HashMap<String, String>();
+				mobileEmulation.put("deviceName", deviceName);
+
+				Map<String, Object> chromeOptions = new HashMap<String, Object>();
+				chromeOptions.put("mobileEmulation", mobileEmulation);
+				DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+				capabilities.setCapability(ChromeOptions.CAPABILITY,
+						chromeOptions);
+				driver = new ChromeDriver(capabilities);
+				driver.manage().timeouts()
+						.pageLoadTimeout(120, TimeUnit.SECONDS);
+				driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+				driver.manage().timeouts()
+						.setScriptTimeout(60, TimeUnit.SECONDS);
+
+			} else if (Browser.IE.getBrowser().equalsIgnoreCase(browser)) {
+				DesiredCapabilities cap = new DesiredCapabilities();
+				cap.setJavascriptEnabled(true);
+				cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+				driver = new RemoteWebDriver(new URL("http://localhost:5555"),
+						cap);
+				driver.manage().timeouts()
+						.pageLoadTimeout(120, TimeUnit.SECONDS);
+				driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+				driver.manage().timeouts()
+						.setScriptTimeout(60, TimeUnit.SECONDS);
+				driver.manage().window().maximize();
+			} else if (Browser.OPERA.getBrowser().equalsIgnoreCase(browser)) {
+				DesiredCapabilities cap = DesiredCapabilities.operaBlink();
+				cap.setBrowserName("opera");
+				OperaOptions options = new OperaOptions();
+				options.setBinary("/usr/bin/opera");
+				options.addArguments("--ignore-certificate-errors");
+				cap.setCapability(OperaOptions.CAPABILITY, options);
+				OperaDriverService service = new OperaDriverService.Builder()
+						.usingDriverExecutable(
+								new File(
+										StringUtils.isNotEmpty(webDriverPath) ? webDriverPath
+												: "/usr/local/bin/operadriver"))
+						.usingAnyFreePort().build();
+				service.start();
+				driver = new RemoteWebDriver(service.getUrl(), cap);
+				driver.manage().timeouts()
+						.pageLoadTimeout(120, TimeUnit.SECONDS);
+				driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+				driver.manage().timeouts()
+						.setScriptTimeout(60, TimeUnit.SECONDS);
+				driver.manage().window().maximize();
+			} else if (Browser.ANDROID.getBrowser().equalsIgnoreCase(browser)) {
+				driver = new RemoteWebDriver(DesiredCapabilities.android());
+			} else {
+				String binaryPath = System
+						.getenv(Constants.BROWSER_BINARY_PATH);
+				String driverPath = System.getenv(Constants.WEB_DRIVER_PATH);
+				if (StringUtils.isNotEmpty(driverPath))
+					System.setProperty("webdriver.gecko.driver", driverPath);
+				else
+					System.out
+							.println("Test is using default driver path for execution !!!");
+				File pathToBinary = null;
+				FirefoxBinary firefoxBinary = null;
+				if (StringUtils.isNotEmpty(binaryPath))
+					try {
+						pathToBinary = new File(binaryPath);
+					} catch (Exception e) {
+						pathToBinary = null;
+						e.printStackTrace();
+					}
+				else
+					System.out
+							.println("Test is using default firefox binary path for execution !!!");
+				firefoxBinary = pathToBinary != null ? new FirefoxBinary(
+						pathToBinary) : new FirefoxBinary();
+				FirefoxProfile profile = new FirefoxProfile();
+				String isUPA = System.getenv(Constants.IS_UPA);
+				if ("true".equalsIgnoreCase(isUPA)) {
+					File netexport = new File(this.getClass().getClassLoader()
+							.getResource("har/netExport-0.8.xpi").toURI());
+					File firebug = new File(this.getClass().getClassLoader()
+							.getResource("har/firebug-2.0.13.xpi").toURI());
+					profile.addExtension(netexport);
+					profile.addExtension(firebug);
+					profile.setPreference("app.update.enabled", false);
+					String domain = "extensions.firebug.";
+					profile.setPreference(domain + "currentVersion", "2.0.13");
+					profile.setPreference(domain + "allPagesActivation", "on");
+					profile.setPreference(domain + "defaultPanelName", "net");
+					profile.setPreference(domain + "net.enableSites", true);
+					profile.setPreference(domain
+							+ "netexport.alwaysEnableAutoExport", true);
+					profile.setPreference(domain + "netexport.showPreview",
+							false);
+					File harFolder = new File(System.getProperty("user.dir")
+							+ "/target/reports/har/");
+					profile.setPreference(domain + "netexport.defaultLogDir",
+							harFolder.getAbsolutePath());
+				}
+				FirefoxOptions firefoxOptions = new FirefoxOptions();
+				firefoxOptions.setBinary(firefoxBinary);
+				firefoxOptions.setProfile(profile);
+				driver = new FirefoxDriver(firefoxOptions);
+				driver.manage().timeouts()
+						.pageLoadTimeout(120, TimeUnit.SECONDS);
+				driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+				driver.manage().timeouts()
+						.setScriptTimeout(60, TimeUnit.SECONDS);
+				driver.manage().window().maximize();
+				reporter = new UPAReporter();
+			}
+
 			Thread.sleep(5000);
-		} catch (InterruptedException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			if (driver == null) {
+				System.out.println("Driver not found !!!");
+				System.exit(1);
+			}
 		}
 	}
 
 	// This method kills the browser after the test is over
 	// It also takes a screenshot and embeds it in the report if the test fails
 	// This method is a hook which runs after every test
-	@After 
+	@After
 	public void afterEach(Scenario scenario) {
-		if (scenario.isFailed()) {
-			try {
+		try {
+			if (scenario.isFailed()) {
 				scenario.write("Current Page URL is " + driver.getCurrentUrl());
 				byte[] screenshot = ((TakesScreenshot) driver)
 						.getScreenshotAs(OutputType.BYTES);
 				scenario.embed(screenshot, "image/png");
-			} catch (WebDriverException somePlatformsDontSupportScreenshots) {
-				System.err.println(somePlatformsDontSupportScreenshots
-						.getMessage());
+
 			}
+			String isUPA = System.getenv(Constants.IS_UPA);
+			if ("true".equalsIgnoreCase(isUPA)) {
+				String scenarioName = scenario.getName().replace(" ", "_");
+				File harFolder = new File(System.getProperty("user.dir")
+						+ "/target/reports/har/");
+				File[] listOfFiles = harFolder.listFiles();
+				int cnt = 0;
+				ArrayList<String> upaLinks = new ArrayList<String>();
+				for (int i = 0; i < listOfFiles.length; i++) {
+					File file = listOfFiles[i];
+					if (file.getName().contains("+")) {
+						String htmlName = scenarioName + "_" + cnt++;
+						File newFile = new File(file.getParent(), htmlName
+								+ ".har");
+						file.renameTo(newFile);
+						String simpleHarExec = "simplehar "
+								+ newFile.getAbsolutePath() + " "
+								+ newFile.getParent() + File.separator
+								+ htmlName + ".html";
 
-		}
-		if (System.getenv("IS_UPA").equalsIgnoreCase("true")) {
-			String scenarioName = scenario.getName().replace(" ", "_");
-			File harFolder = new File(System.getProperty("user.dir") + "/target/reports/har/");
-			File[] listOfFiles = harFolder.listFiles();
-			int cnt = 0;
-			ArrayList<String> upaLinks = new ArrayList<String>();
-			for (int i = 0; i < listOfFiles.length; i++) {
-				File file = listOfFiles[i];
-				if (file.getName().contains("+")) {
-					String htmlName = scenarioName + "_" + cnt++;
-					File newFile = new File(file.getParent(), htmlName + ".har");
-					file.renameTo(newFile);
-					String simpleHarExec = "simplehar " + newFile.getAbsolutePath() + " " + newFile.getParent()
-							+ File.separator + htmlName + ".html";
-
-					try {
 						Runtime.getRuntime().exec(simpleHarExec);
-					} catch (IOException e) {
-						System.out.println("Unable to execute Simplehar");
-						e.printStackTrace();
+						upaLinks.add(htmlName + ".html");
 					}
-					upaLinks.add(htmlName + ".html");
 				}
+				UPAResult result = new UPAResult();
+				result.setScenarioName(scenarioName);
+				result.setUpaReportLinks(upaLinks);
+				reporter.addDetails(result);
+				reporter.writeResults();
 			}
-			UPAResult result = new UPAResult();
-			result.setScenarioName(scenarioName);
-			result.setUpaReportLinks(upaLinks);
-			reporter.addDetails(result);
-			reporter.writeResults();
+		} catch (Exception e) {
+
+		} finally {
+			if (driver != null)
+				driver.quit();
 		}
-		driver.quit();
-		
+
 	}
 
 }
